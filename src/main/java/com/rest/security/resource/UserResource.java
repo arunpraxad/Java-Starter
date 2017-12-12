7b044a18a066422d02087765cde38e924146ad91
arunpraxad <arunpraxad@gmail.com>
2017-12-12 21:42:26 +0530

    spring security

---
+++ src/main/java/com/rest/security/resource/UserResource.java
@@ -0,0 +1,22 @@
+package com.rest.security.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+    @RequestMapping(value = "/test")
+    String getTest() {
+        return "Hi!!!";
+    }
+
+    @RequestMapping("/users")
+    public @ResponseBody String getUsers() {
+        return "{\"users\":[{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
+                "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}]}";
+    }
+
+
+}
